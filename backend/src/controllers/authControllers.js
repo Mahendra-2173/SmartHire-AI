cf771c9d6db8057afb915d6252ff35091322f0e9
Mahendra-deadpool <kuruva_ug_23@cse.nits.ac.in>
2026-05-31 12:31:13 +0530

    Fix production authentication cookies

--- backend/src/controllers/authControllers.js
+++ backend/src/controllers/authControllers.js
@@ -70,12 +70,12 @@ const loginUser = async (req, res) => {
     expiresIn: "1d",
   });
 
-  res.cookie("token", token, {
-    httpOnly: true,
-    secure: false,
-    sameSite: "lax",
-    maxAge: 24 * 60 * 60 * 1000,
-  });
+ res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",
+  maxAge: 24 * 60 * 60 * 1000,
+});
 
   res.status(200).json({ message: "User logged in successfully", user });
 };
