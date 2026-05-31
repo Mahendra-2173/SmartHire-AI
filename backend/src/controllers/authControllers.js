9334fe34d48d96ffa08790e3fd9be3ff08670fe4
Mahendra-deadpool <kuruva_ug_23@cse.nits.ac.in>
2026-05-31 21:33:08 +0530

    fix: cookie settings for production

--- backend/src/controllers/authControllers.js
+++ backend/src/controllers/authControllers.js
@@ -33,10 +33,11 @@ const registerUser = async (req, res) => {
     expiresIn: "1D",
   });
 
-  res.cookie("token", token, {
+ res.cookie("token", token, {
   httpOnly: true,
-  secure: false,
-  sameSite: "lax",
+  secure: true,
+  sameSite: "none",
+  maxAge: 24 * 60 * 60 * 1000,
 });
 
   res.status(201).json({ message: "User registered successfully", user });
@@ -92,7 +93,11 @@ const logoutUser = async (req, res) => {
   if (token) {
     await blacklistTokenmodel.create({ token });
   }
-  res.clearCookie("token");
+  res.clearCookie("token", {
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",
+});
   res.status(200).json({ message: "User logged out successfully" });
 };
 
